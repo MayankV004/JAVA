52e57f35cb647dd22a68987210558abc86999b5b
MayankV004 <abcdef.msverma@gmail.com>
2024-06-26 01:52:27 +0530

    added few more content

--- DSA/Bit Manipulation/BitwiseOperator.java
+++ DSA/Bit Manipulation/BitwiseOperator.java
@@ -6,8 +6,12 @@ class BitwiseOperator
         int b = a >> 1; // right shift 
         int c = a << 1; // left shift
         int d = ~a; // two's complement
+        int e = a >>> 1 ; // unsigned right shift
+       
         System.out.println(b);
         System.out.println(c);
         System.out.println(d);
+        System.out.println(e);
+        
     }
 }
